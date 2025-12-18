d214f431aa6f26df575d4ce51a5ae39a889a4ff7
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-12-18 23:11:01 +0300

    .

--- Tyuiu.RadochinaAP.Sprint7.Project.V5/FormMain.Designer.cs
+++ Tyuiu.RadochinaAP.Sprint7.Project.V5/FormMain.Designer.cs
@@ -2,38 +2,52 @@
 {
     partial class FormMain
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            // ... другой код выше ...
+
+            // richTextBoxCondition_PAA
+            this.richTextBoxCondition_PAA = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxCondition_PAA.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBoxCondition_PAA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.richTextBoxCondition_PAA.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxCondition_PAA.ForeColor = System.Drawing.Color.Black;
+            this.richTextBoxCondition_PAA.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxCondition_PAA.Name = "richTextBoxCondition_PAA";
+            this.richTextBoxCondition_PAA.ReadOnly = true;
+            this.richTextBoxCondition_PAA.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBoxCondition_PAA.Size = new System.Drawing.Size(884, 150);
+            this.richTextBoxCondition_PAA.TabIndex = 0;
+
+            // ВОТ ЭТУ СТРОКУ ДОБАВЬТЕ - ТЕКСТ УСЛОВИЯ
+            this.richTextBoxCondition_PAA.Text = "УСЛОВИЕ ЗАДАЧИ (Вариант 11):\n" +
+                "--------------------------------------------------\n" +
+                "Дан файл InPutFileTask7V11.csv в котором хранится матрица целочисленных значений.\n\n" +
+                "ТРЕБОВАНИЯ:\n" +
+                "1. Загрузить файл через диалоговое окно\n" +
+                "2. Изменить в пятой строке матрицы все отрицательные числа на 9\n" +
+                "3. Результат вывести в правую таблицу\n" +
+                "4. Сохранить результат в CSV файл\n\n" +
+                "ИСПОЛЬЗУЕМАЯ БИБЛИОТЕКА:\n" +
+                "• DataService.GetMatrix(string path) - загрузка матрицы из файла\n\n" +
+                "АЛГОРИТМ РАБОТЫ:\n" +
+                "1. Нажмите 'Загрузить файл' → выберите CSV файл\n" +
+                "2. Нажмите 'Преобразовать' → выполняется замена в 5-й строке\n" +
+                "3. Нажмите 'Сохранить результат' → сохраняете изменения\n" +
+                "4. Нажмите 'Справка' → информация о разработчике\n\n" +
+                "ФОРМАТ ФАЙЛА:\n" +
+                "• Разделитель - точка с запятой (;)\n" +
+                "• Пример: 1;2;-3;4;5\n" +
+                "      6;-7;8;9;-10";
+
+            // ... продолжение кода ниже ...
+
+            // Добавление RichTextBox на форму
+            this.Controls.Add(this.richTextBoxCondition_PAA);
+
+            // ... остальной код формы ...
         }
 
-        #endregion
+        private System.Windows.Forms.RichTextBox richTextBoxCondition_PAA;
     }
 }
--- Tyuiu.RadochinaAP.Sprint7.Project.V5/FormMain.cs
+++ Tyuiu.RadochinaAP.Sprint7.Project.V5/FormMain.cs
@@ -6,6 +6,45 @@ namespace Tyuiu.RadochinaAP.Sprint7.Project.V5
         public FormMain()
         {
             InitializeComponent();
+            SetConditionText();
+        }
+
+        private void SetConditionText()
+        {
+            // Очищаем старый текст
+            richTextBoxCondition_PAA.Clear();
+
+            // Добавляем форматированный текст
+            richTextBoxCondition_PAA.AppendText("УСЛОВИЕ ЗАДАЧИ (Вариант 11):\n");
+            richTextBoxCondition_PAA.AppendText("--------------------------------------------------\n");
+            richTextBoxCondition_PAA.AppendText("Дан файл InPutFileTask7V11.csv в котором хранится матрица целочисленных значений.\n\n");
+
+            richTextBoxCondition_PAA.AppendText("ТРЕБОВАНИЯ:\n");
+            richTextBoxCondition_PAA.AppendText("1. Загрузить файл через диалоговое окно\n");
+            richTextBoxCondition_PAA.AppendText("2. Изменить в пятой строке матрицы все отрицательные числа на 9\n");
+            richTextBoxCondition_PAA.AppendText("3. Результат вывести в правую таблицу\n");
+            richTextBoxCondition_PAA.AppendText("4. Сохранить результат в CSV файл\n\n");
+
+            richTextBoxCondition_PAA.AppendText("ИСПОЛЬЗУЕМАЯ БИБЛИОТЕКА:\n");
+            richTextBoxCondition_PAA.AppendText("• DataService.GetMatrix(string path) - загрузка матрицы из файла\n\n");
+
+            richTextBoxCondition_PAA.AppendText("АЛГОРИТМ РАБОТЫ:\n");
+            richTextBoxCondition_PAA.AppendText("1. Нажмите 'Загрузить файл' ? выберите CSV файл\n");
+            richTextBoxCondition_PAA.AppendText("2. Нажмите 'Преобразовать' ? выполняется замена в 5-й строке\n");
+            richTextBoxCondition_PAA.AppendText("3. Нажмите 'Сохранить результат' ? сохраняете изменения\n");
+            richTextBoxCondition_PAA.AppendText("4. Нажмите 'Справка' ? информация о разработчике\n\n");
+
+            richTextBoxCondition_PAA.AppendText("ФОРМАТ ФАЙЛА:\n");
+            richTextBoxCondition_PAA.AppendText("• Разделитель - точка с запятой (;)\n");
+            richTextBoxCondition_PAA.AppendText("• Пример: 1;2;-3;4;5\n");
+            richTextBoxCondition_PAA.AppendText("      6;-7;8;9;-10");
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            // Если нужно что-то выполнить при загрузке формы
+            // Оставьте пустым или добавьте другую логику
         }
     }
 }
+       
